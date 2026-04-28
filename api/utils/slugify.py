5868e80b9722f1da17bb4d35236f0572bb57204d
projetosfernandescompany-debug <projetosfernandescompany@gmail.com>
2026-04-28 10:26:03 -0300

    Update slugify.py

--- api/utils/slugify.py
+++ api/utils/slugify.py
@@ -30,7 +30,7 @@ def data_card():
             "nome": base,
             "url_p": f"card/{url}"
         })
-    
+    print(dados)
     if dados: return dados
 
 def data_card_computer_architecture():
