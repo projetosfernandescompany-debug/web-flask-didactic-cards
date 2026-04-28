03e4df352e9c7aef844fbff8da5f05ca12a7c626
projetosfernandescompany-debug <projetosfernandescompany@gmail.com>
2026-04-28 11:18:48 -0300

    Update card.py

--- api/routes/card.py
+++ api/routes/card.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from flask import Blueprint, render_template
 from utils.slugify import data_card_computer_architecture
 
@@ -11,8 +12,13 @@ def arquitetura_de_computadores():
 
 @card_bp.route('/componentes-de-hardware')
 def componentes_de_hardware():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Componentes de hardware.csv")
+    
     list_quest = []
-    with open("./api/file/Componentes de hardware.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
@@ -20,8 +26,13 @@ def componentes_de_hardware():
 
 @card_bp.route('/computacao-em-nuvem')
 def computacao_em_nuvem():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Computação em Nuvem.csv")
+    
     list_quest = []
-    with open("./api/file/Computação em Nuvem.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
@@ -29,8 +40,13 @@ def computacao_em_nuvem():
 
 @card_bp.route('/fundamentos-de-redes-de-computadores')
 def fundamentos_de_redes_de_computadores():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Fundamentos de Redes de Computadores.csv")
+    
     list_quest = []
-    with open("./api/file/Fundamentos de Redes de Computadores.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
@@ -38,8 +54,13 @@ def fundamentos_de_redes_de_computadores():
 
 @card_bp.route('/introducao-a-programacao-de-computadores-com-c')
 def introducao_a_programacao_de_computadores_com_c():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Introdução à Programação de Computadores com C.csv")
+    
     list_quest = []
-    with open("./api/file/Introdução à Programação de Computadores com C.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
@@ -47,8 +68,13 @@ def introducao_a_programacao_de_computadores_com_c():
 
 @card_bp.route('/introducao-a-seguranca-da-informacao')
 def introducao_a_seguranca_da_informacao():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Introdução à Segurança da Informação.csv")
+    
     list_quest = []
-    with open("./api/file/Introdução à Segurança da Informação.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
@@ -56,9 +82,15 @@ def introducao_a_seguranca_da_informacao():
 
 @card_bp.route('/pensamento-computacional')
 def pensamento_computacional():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    DIR = os.path.join(BASE_DIR, "file")
+
+    FILE_PATH = os.path.join(DIR, "Pensamento Computacional.csv")
+    
     list_quest = []
-    with open("./api/file/Pensamento Computacional.csv", newline='', encoding="utf-8") as f:
+    with open(FILE_PATH, newline='', encoding="utf-8") as f:
         file = csv.reader(f)
         for row in file:
             list_quest.append(row)
     return render_template('card/pensamento-computacional.html', quest=list_quest)
+
